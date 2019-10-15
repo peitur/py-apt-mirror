9f9113bf935e7ea8d7d565abdaaeac431d3fc4ed
peitur <peitur@gmail.com>
2019-10-15 08:24:51 +0200

    Added htob and btoh size calculations functions

--- aptmirror/utils.py
+++ aptmirror/utils.py
@@ -17,5 +17,39 @@ def random_string( length ):
     return ''.join(random.SystemRandom().choice( string.ascii_lowercase + string.ascii_uppercase + string.digits) for _ in range( length ))
 
 
+def size_htob( s ):
+    rx = re.compile( r"([0-9\.]+)([bkMG]*)[bB]*" )
+    m = rx.match( s )
+    nbytes = None
+
+    if m:
+        amount = m.group(1)
+        unit = m.group(2)
+
+        if unit == "k":
+            nbytes = int( float( amount ) * 1024 )
+        elif unit == "M":
+            nbytes = int( float( amount ) * 1024 * 1024 )
+        elif unit == "G":
+            nbytes = int( float( amount ) * 1024 * 1024 * 1024 )
+        else:
+            if re.match( r"[0-9]+\.[0-9]+", amount ):
+                raise AttributeError("Bad byte format. Got float value!")
+            nbytes = int( amount )
+    return int( nbytes )
+
+
+def size_btoh( s ):
+
+    units = { "": 1, "k": 1024, "M": 1024 * 1024, "G": 1024 * 1024 * 1024  }
+
+    x = int( s )
+    for i in units:
+        c = float( x / units[i] )
+        if c < 500:
+            return "%s%s" % ( c, i )
+    return x
+
+
 if __name__ == "__main__":
     pass
